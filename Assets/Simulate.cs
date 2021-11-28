d4a9bfc685a3f9b0b93bfe5939ddd1e00452f9c0
Anton <Worgene@yandex.ru>
2021-11-29 00:15:22 +0300

    init

---
+++ Assets/Simulate.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulate : MonoBehaviour
+{
+
+    readonly float G = 6.7f;
+    public float a = 10;
+    GameObject[] celestials;
+    // Start is called before the first frame update
+    void Start()
+    {
+        celestials = GameObject.FindGameObjectsWithTag("Celestial");
+        InitialVelocity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+    void FixedUpdate()
+    {
+        Gravity();
+    }
+    void Gravity()
+    {
+        foreach (GameObject first in celestials)
+        {
+            foreach (GameObject second in celestials)
+            {
+                if (!first.Equals(second))
+                {
+                    float m1 = first.GetComponent<Rigidbody>().mass;
+                    float m2 = second.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(first.transform.position, second.transform.position);
+
+                    first.GetComponent<Rigidbody>().AddForce((second.transform.position - first.transform.position).normalized * (G * (m1 * m2) / (r * r)));
+                    Debug.Log($" FirstObj={first.name} Second = {second.name} Object Radius = {r}, AddForce X = {first.transform.position.x}, AddForce Y = {first.transform.position.y}");
+                }
+            }
+        }
+    }
+    void InitialVelocity()
+    {
+        foreach (GameObject first in celestials)
+        {
+            foreach (GameObject second in celestials)
+            {
+                if (!first.Equals(second))
+                {
+                    float m2 = second.GetComponent <Rigidbody>().mass;
+                    float r = Vector3.Distance(first.transform.position, second.transform.position);
+                    first.transform.LookAt(second.transform);
+                   
+                    first.GetComponent<Rigidbody>().velocity += first.transform.right * Mathf.Sqrt((G * m2) / r);
+                    Debug.Log($"Object = {first.name} ");
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Simulation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulation : MonoBehaviour
+{
+
+
+    public float gravity = 6.73f;
+    public float sunMass = 100; // maybe too much?
+    public float sphereMassScale = 2; // depends on actual scale of spheres
+    public float initForceScale = 100;
+    public List<GameObject> spheres;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject s in spheres)
+        {
+            s.GetComponent<Rigidbody>().AddForce(Random.insideUnitSphere * initForceScale, ForceMode.Acceleration);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Loop through each sphere totaling up and applying gravitational forces
+        foreach (GameObject s in spheres)
+        {
+            Vector3 totalForce = new Vector3(0, 0, 0), forceDirection;
+            float forceAmount;
+            float sphereMass = s.transform.localScale.x * sphereMassScale;
+
+            // first calculate sun's gravity by multiplying direction and force amount
+            forceDirection = (transform.position - s.transform.position).normalized;
+            forceAmount = getForce(sphereMass, sunMass, Vector3.Distance(transform.position, s.transform.position));
+            totalForce += forceDirection * forceAmount;
+
+            // this is optional, use if you want sphere gravity to affect each other (moons)
+            foreach (GameObject p in spheres)
+            {
+                if (!p.Equals(s))
+                { // don't check itself
+                    forceDirection = (p.transform.position - s.transform.position).normalized;
+                    forceAmount = getForce(sphereMass, p.transform.localScale.x * sphereMassScale, Vector3.Distance(p.transform.position, s.transform.position));
+                    totalForce += forceDirection * forceAmount;
+                }
+            }
+           // s.GetComponent<Rigidbody>().AddForce(totalForce, ForceMode.Acceleration);
+        }
+    }
+    float getForce(float mass1, float mass2, float distance)
+    {
+        // return F=(Gm2m2)/d^2
+        return (gravity * mass1 * mass2) / Mathf.Pow(distance, 2);
+    }       // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
